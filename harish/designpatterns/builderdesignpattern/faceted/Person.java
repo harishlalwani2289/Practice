238111a927061c3fee2e9844ef38c713a3c483ea
Harish Lalwani <harishlalwani2289@gmail.com>
2021-05-25 11:03:58 +0530

    Faceted Builder with Person, Person Job, Person Address builder all working together to build a person object

---
+++ harish/designpatterns/builderdesignpattern/faceted/Person.java
@@ -0,0 +1,21 @@
+package practice.harish.designpatterns.builderdesignpattern.faceted;
+
+public class Person {
+
+    public String streetAddress, postcode, city;
+
+    public String companyName, position;
+    public int annualIncome;
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "streetAddress='" + streetAddress + '\'' +
+                ", postcode='" + postcode + '\'' +
+                ", city='" + city + '\'' +
+                ", companyName='" + companyName + '\'' +
+                ", position='" + position + '\'' +
+                ", annualIncome=" + annualIncome +
+                '}';
+    }
+}
---
+++ harish/designpatterns/builderdesignpattern/faceted/PersonBuilder.java
@@ -0,0 +1,21 @@
+package practice.harish.designpatterns.builderdesignpattern.faceted;
+
+// facade Builder
+public class PersonBuilder {
+
+    protected Person person = new Person();
+
+
+    public Person build() {
+        return person;
+    }
+
+    public PersonAddressBuilder lives() {
+        return new PersonAddressBuilder(person);
+    }
+
+    public PersonJobBuilder works() {
+        return new PersonJobBuilder(person);
+    }
+
+}
