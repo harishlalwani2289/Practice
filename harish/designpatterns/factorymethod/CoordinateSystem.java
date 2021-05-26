276a1322b6c97a13ac080553b14cd090633e10e9
Harish Lalwani <harishlalwani2289@gmail.com>
2021-05-26 11:43:47 +0530

    Factory method of coordinate system.

---
+++ harish/designpatterns/factorymethod/CoordinateSystem.java
@@ -0,0 +1,6 @@
+package practice.harish.designpatterns.factorymethod;
+
+public enum CoordinateSystem {
+    POLAR,
+    CARTESIAN
+}
---
+++ harish/designpatterns/factorymethod/Demo.java
@@ -0,0 +1,8 @@
+package practice.harish.designpatterns.factorymethod;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        Point point = Point.newCartesianPoint(2,3);
+    }
+}
