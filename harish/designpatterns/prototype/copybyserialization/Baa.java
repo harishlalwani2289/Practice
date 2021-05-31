d88c032352951f77faa939ed919d803882bec79c
Harish Lalwani <harishlalwani2289@gmail.com>
2021-05-31 12:05:28 +0530

    Prototype design pattern

---
+++ harish/designpatterns/prototype/copybyserialization/Baa.java
@@ -0,0 +1,38 @@
+package practice.harish.designpatterns.prototype.copybyserialization;
+
+import java.io.Serializable;
+
+public class Baa implements Serializable {
+
+    public Foo foo;
+    public int baValue;
+
+    public Baa(Foo foo, int baValue) {
+        this.foo = foo;
+        this.baValue = baValue;
+    }
+
+    public Foo getFoo() {
+        return foo;
+    }
+
+    public void setFoo(Foo foo) {
+        this.foo = foo;
+    }
+
+    public int getBaValue() {
+        return baValue;
+    }
+
+    public void setBaValue(int baValue) {
+        this.baValue = baValue;
+    }
+
+    @Override
+    public String toString() {
+        return "Baa{" +
+                "foo=" + foo +
+                ", baValue=" + baValue +
+                '}';
+    }
+}
