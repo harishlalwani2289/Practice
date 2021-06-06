64632b14d198814240adee297f1b0e2765379e40
Harish Lalwani <harishlalwani2289@gmail.com>
2021-06-06 17:55:11 +0530

    Inner static Implementation of Singleton Object

---
+++ harish/designpatterns/interfacesegregationprinciple/InnerStaticSingleton.java
@@ -0,0 +1,14 @@
+package practice.harish.designpatterns.interfacesegregationprinciple;
+
+public class InnerStaticSingleton {
+
+    private InnerStaticSingleton(){}
+
+    private static class Impl {
+        private static final InnerStaticSingleton INSTANCE = new InnerStaticSingleton();
+    }
+
+    public static InnerStaticSingleton getInstance() {
+        return Impl.INSTANCE;
+    }
+}
